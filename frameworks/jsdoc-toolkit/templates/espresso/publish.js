c55b0f1ea31eb4ee62e66718bf1870c67fd02be9
Tim Evans <tim.evans@junctionnetworks.com>
2011-01-25 20:40:16 -0500

    minor tweaks to doc styles

--- frameworks/jsdoc-toolkit/templates/espresso/publish.js
+++ frameworks/jsdoc-toolkit/templates/espresso/publish.js
@@ -68,7 +68,6 @@ function publish(symbolSet) {
 	// create each of the class pages
 	for (i = 0, l = classes.length; i < l; i++) {
 		var symbol = classes[i];
-
 		symbol.events = symbol.getEvents();   // 1 order matters
 		symbol.methods = symbol.getMethods(); // 2
 
